8c22ea1c27579db48c3ada918d22993c99452f5b
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2025-11-15 01:45:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pickle
 
+
 # Load model & scaler
 model = pickle.load(open("diabetes_model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
